aa5b2254c878f1541c373553eaf1f5834aeac0e7
KinMan Zhang <2376494700@qq.com>
2024-02-28 11:57:17 +0800

    api,pinia,axios

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import router from './router';
 // 引入组件库的少量全局样式变量
 import 'tdesign-vue-next/es/style/index.css';
 import './assets/css/theme.css'
-
+//导入pinia
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+const pinia = createPinia().use(piniaPluginPersistedstate)
 const app = createApp(App);
-app.use(TDesign);
-app.use(router)
-app.mount('#app')
+app.use(TDesign).use(router).use(pinia).mount('#app');
