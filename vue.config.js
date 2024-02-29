d7eda184c7657220ab2c12330882d842ab6d5ec4
KinMan Zhang <2376494700@qq.com>
2024-02-29 16:07:19 +0800

    实现登录注册跳转

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-export default defineConfig({
-    server: {
-        proxy: {
-          '/api': {
-            target: 'http://192.168.17.169:8080',//后端服务器
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-          }
-        }
-      }
+  transpileDependencies: true,
 })
