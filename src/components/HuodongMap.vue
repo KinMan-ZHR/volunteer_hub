811d3cd59c8600a06ba2bb3ce65daa9cd74f0d95
= <=>
2024-03-01 15:42:17 +0800

    update map

--- src/components/HuodongMap.vue
+++ src/components/HuodongMap.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="width: 100%; display: flex;justify-content: center;;">
+    <div style="width: 100%; display: flex;justify-content: center;margin-top: 15px;">
         <div id="province" style="width: 850px;height: 850px;z-index: 999;"></div>
     </div>
 </template>
@@ -7,10 +7,15 @@
 <script>
 import * as echarts from "echarts/core";
 import {CanvasRenderer} from "echarts/renderers";
-import {MapChart} from "echarts/charts";
+import {MapChart,ScatterChart} from "echarts/charts";
+import { LegendComponent, TitleComponent,TooltipComponent } from 'echarts/components';
+import MyTooltip from './MyTooltip.vue';
+import { createApp } from "vue";
+
 import $ from 'jquery';
 
-echarts.use([MapChart]);
+echarts.use([MapChart,ScatterChart ]);
+echarts.use([TitleComponent,LegendComponent,TooltipComponent ]);
 
 export default{
     data(){
@@ -23,6 +28,7 @@ export default{
                     img:'https://tdesign.gtimg.com/site/source/card-demo.png',
                     description:'这是一段描述',
                     state:1,
+                    link:'http://sc.chinavolunteer.mca.gov.cn/subsite/sichuan/projectInfo/b0f1f7d451474e07bc5ddb10070f1dc5/511323000000000000'
                 },
                 {
                     marker:[104.046,32.106],
@@ -31,15 +37,22 @@ export default{
                     img:'https://tdesign.gtimg.com/site/source/card-demo.png',
                     description:'这是第二段描述',
                     state:0,
+                    link:'http://sc.chinavolunteer.mca.gov.cn/subsite/sichuan/projectInfo/b0f1f7d451474e07bc5ddb10070f1dc5/511323000000000000'
                 },
                 {
-                    marker:[104.046,32.106],
-                    title:'乡村留守儿童',
+                    marker:[104.046,33.106],
+                    title:'留守儿童',
                     location:'四川省雅安市',
                     img:'https://tdesign.gtimg.com/site/source/card-demo.png',
                     description:'这是第二段描述',
                     state:2,
+                    link:'http://sc.chinavolunteer.mca.gov.cn/subsite/sichuan/projectInfo/b0f1f7d451474e07bc5ddb10070f1dc5/511323000000000000'
                 },
+            ],
+            legend:[
+                {name:'进行中',color:'#48c79c'},
+                {name:'已结项',color:'#a6a6a6'},
+                {name:'未开始',color:'#f2995f'}
             ],
             map_path:'https://geo.datav.aliyun.com/areas_v3/bound/510000_full.json'
 
@@ -65,6 +78,8 @@ export default{
                 maskColor:'#08204200' //加载时候的背景颜色
             });
 
+            var _this = this
+
             $.get(path,function(usaJson){
                 // console.log(usaJson);
                 myChart.hideLoading();
@@ -79,26 +94,44 @@ export default{
                         show:true,
                         trigger: 'item',
                         showDelay: 0,
+                        enterable: true,
+                        hideDelay: 200,
                         transitionDuration: 0.2,
                         formatter: function(params) {
-                            // console.log('params',params);
-                            // return params.name + ': ' + params.value[2];
-                            return params.data.title
-                        }
+                            const title = params.data.name
+                            const img = params.data.img
+                            const state = params.data.state
+                            const link = params.data.link
+                            // 容器，之后会把组件渲染在容器中
+                            const div = document.createElement('div')
+                            // vue文件直接用不行，得创建app实例,同时提供组件的props
+                            const app = createApp(MyTooltip, {title, img, state, link})
+                            // 将app实例挂载到dom上
+                            app.mount(div)
+                            // 将含有组件实例的dom返回给echats
+                            return div
+
+                        },
+
+                        position: (point) => {
+                            let pointX = point[0];
+                            let pointY = point[1];
+                            return [pointX, pointY];
+                        },
                     },
-                    // legend: {
-                    //     data: province_geo_data[index].legend.map(function(item){
-                    //         // console.log(item);
-                    //         return{
-                    //             name:item.name,
-                    //             itemStyle:{
-                    //                 color:item.color
-                    //             }
-                    //         }
-                    //     }),
-                    //     left: 'center',
-                    //     bottom: 5,
-                    // },
+                    legend: {
+                        data: _this.legend.map(function(item){
+                            // console.log(item);
+                            return{
+                                name:item.name,
+                                itemStyle:{
+                                    color:item.color
+                                }
+                            }
+                        }),
+                        left: 'center',
+                        bottom: 5,
+                    },
                     geo:{
                         map:'SC',
                         itemStyle: {
@@ -147,152 +180,94 @@ export default{
 
                         }
                     },
-                    // series:[
-                    //     {
-                    //         name:'绿茶',
-                    //         type:'scatter',
-                    //         emphasis: {
-                    //             focus: 'series'
-                    //         },
-                    //         coordinateSystem: 'geo',
-                    //         data:province_geo_data[index].value.map(function(item){
-                    //             if(item.type == '绿茶'){
-                    //                 return{
-                    //                     name:item.location_name,
-                    //                     value: item.marker,
-                    //                     itemStyle:{ color: item.color}
-                    //                 }
-                    //             }
-                    //         }),
-                    //         itemStyle:{
-                    //             borderColor: '#ffffff', // 设置描边颜色
-                    //             borderWidth: 2 // 设置描边宽度
-                    //         },
+                    series:[
 
-                    //         symbolSize:10,
+                        {
+                            name:'进行中',
+                            type:'scatter',
+                            emphasis: {
+                                focus: 'series'
+                            },
+                            coordinateSystem: 'geo',
+                            data:_this.province_geo_data.map(function(item){
+                                if(item.state == 1){
+                                    return{
+                                        name:item.title,
+                                        value: item.marker,
+                                        img:item.img,
+                                        description:item.description,
+                                        state:item.state,
+                                        location:item.location,
+                                        link:item.link,
+                                        itemStyle:{ color: _this.legend[0].color}
+                                    }
+                                }
+                            }),
+                            itemStyle:{
+                                borderColor: '#ffffff', // 设置描边颜色
+                                borderWidth: 2 // 设置描边宽度
+                            },
+                            symbolSize:10,
 
-                    //     },
-                    //     {
-                    //         name:'白茶',
-                    //         type:'scatter',
-                    //         emphasis: {
-                    //             focus: 'series'
-                    //         },
-                    //         coordinateSystem: 'geo',
-                    //         data:province_geo_data[index].value.map(function(item){
-                    //             if(item.type == '白茶'){
-                    //                 return{
-                    //                     name:item.location_name,
-                    //                     value: item.marker,
-                    //                     itemStyle:{ color: item.color}
-                    //                 }
-                    //             }
-                    //         }),
-                    //         itemStyle:{
-                    //             borderColor: '#ffffff', // 设置描边颜色
-                    //             borderWidth: 2 // 设置描边宽度
-                    //         },
+                        },
+                        {
+                            name:'已结项',
+                            type:'scatter',
+                            emphasis: {
+                                focus: 'series'
+                            },
+                            coordinateSystem: 'geo',
+                            data:_this.province_geo_data.map(function(item){
+                                if(item.state == 0){
+                                    return{
+                                        name:item.title,
+                                        value: item.marker,
+                                        img:item.img,
+                                        description:item.description,
+                                        state:item.state,
+                                        location:item.location,
+                                        link:item.link,
+                                        itemStyle:{ color: _this.legend[1].color}
+                                    }
+                                }
+                            }),
+                            itemStyle:{
+                                borderColor: '#ffffff', // 设置描边颜色
+                                borderWidth: 2 // 设置描边宽度
+                            },
+                            symbolSize:10,
 
-                    //         symbolSize:10,
+                        },
+                        {
+                            name:'未开始',
+                            type:'scatter',
+                            emphasis: {
+                                focus: 'series'
+                            },
+                            coordinateSystem: 'geo',
+                            data:_this.province_geo_data.map(function(item){
+                                if(item.state == 2){
+                                    return{
+                                        name:item.title,
+                                        value: item.marker,
+                                        img:item.img,
+                                        description:item.description,
+                                        state:item.state,
+                                        location:item.location,
+                                        link:item.link,
+                                        itemStyle:{ color: _this.legend[2].color}
+                                    }
+                                }
+                            }),
+                            itemStyle:{
+                                borderColor: '#ffffff', // 设置描边颜色
+                                borderWidth: 2 // 设置描边宽度
+                            },
+                            symbolSize:10,
 
-                    //     },
-                    //     {
-                    //         name:'黄茶',
-                    //         type:'scatter',
-                    //         emphasis: {
-                    //             focus: 'series'
-                    //         },
-                    //         coordinateSystem: 'geo',
-                    //         data:province_geo_data[index].value.map(function(item){
-                    //             if(item.type == '黄茶'){
-                    //                 return{
-                    //                     name:item.location_name,
-                    //                     value: item.marker,
-                    //                     itemStyle:{ color: item.color}
-                    //                 }
-                    //             }
-                    //         }),
-                    //         itemStyle:{
-                    //             borderColor: '#ffffff', // 设置描边颜色
-                    //             borderWidth: 2 // 设置描边宽度
-                    //         },
+                        },
 
-                    //         symbolSize:10,
-
-                    //     },
-                    //     {
-                    //         name:'乌龙茶',
-                    //         type:'scatter',
-                    //         emphasis: {
-                    //             focus: 'series'
-                    //         },
-                    //         coordinateSystem: 'geo',
-                    //         data:province_geo_data[index].value.map(function(item){
-                    //             if(item.type == '乌龙茶'){
-                    //                 return{
-                    //                     name:item.location_name,
-                    //                     value: item.marker,
-                    //                     itemStyle:{ color: item.color}
-                    //                 }
-                    //             }
-                    //         }),
-                    //         itemStyle:{
-                    //             borderColor: '#ffffff', // 设置描边颜色
-                    //             borderWidth: 2 // 设置描边宽度
-                    //         },
-
-                    //         symbolSize:10,
-
-                    //     },
-                    //     {
-                    //         name:'红茶',
-                    //         type:'scatter',
-                    //         emphasis: {
-                    //             focus: 'series'
-                    //         },
-                    //         coordinateSystem: 'geo',
-                    //         data:province_geo_data[index].value.map(function(item){
-                    //             if(item.type == '红茶'){
-                    //                 return{
-                    //                     name:item.location_name,
-                    //                     value: item.marker,
-                    //                     itemStyle:{ color: item.color}
-                    //                 }
-                    //             }
-                    //         }),
-                    //         itemStyle:{
-                    //             borderColor: '#ffffff', // 设置描边颜色
-                    //             borderWidth: 2 // 设置描边宽度
-                    //         },
-
-                    //         symbolSize:10,
-
-                    //     },
-                    //     {
-                    //         name:'黑茶',
-                    //         type:'scatter',
-                    //         emphasis: {
-                    //             focus: 'series'
-                    //         },
-                    //         coordinateSystem: 'geo',
-                    //         data:province_geo_data[index].value.map(function(item){
-                    //             if(item.type == '黑茶'){
-                    //                 return{
-                    //                     name:item.location_name,
-                    //                     value: item.marker,
-                    //                     itemStyle:{ color: item.color}
-                    //                 }
-                    //             }
-                    //         }),
-                    //         itemStyle:{
-                    //             borderColor: '#ffffff', // 设置描边颜色
-                    //             borderWidth: 2 // 设置描边宽度
-                    //         },
-
-                    //         symbolSize:10,
-                    //     },
-
-                    // ]
+                    ]
                 }
                 myChart.setOption(option);
             })
