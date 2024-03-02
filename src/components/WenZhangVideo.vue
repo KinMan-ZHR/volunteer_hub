fd0c9ac2bc99ad076224ee1cb5fc38d7e0b323ec
= <=>
2024-03-02 15:32:29 +0800

    update gongyiguanggao.vue

--- src/components/WenZhangVideo.vue
+++ src/components/WenZhangVideo.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="all">
+    <div class="video_container">
         <div class="container">
-            <video width="100%" controls>
-                <source :src="link" type="video/mp4">
+            <video width="100%" controls v-if="is_play">
+                <source :src="current_video.link" type="video/mp4">
             </video>
         </div>
-        <div style="width: 16%;margin-left: 20px;">
-            <div class="title">{{ title }}</div>
-            <div class="main">日期：{{ date }}</div>
-            <div class="main">简介：{{ intro }}</div>
+        <div style="margin-left: 20px;">
+            <div class="title">{{ current_video.title }}</div>
+            <div class="main">日期：{{ current_video.date }}</div>
+            <div class="main">简介：{{ current_video.description }}</div>
         </div>
     </div>
 </template>
@@ -16,11 +16,21 @@
 <script>
 export default{
     name:'WenZhangVideo',
+    props:{
+        current_video:{
+            type:Object,
+            required:true
+        },
+        is_play:{
+            type:Boolean,
+            default:false
+        }
+    },
     data(){
         return{
             title:'标题',
             date:'2023-2',
-            intro:'fcesbdifhvskfcbashJiuebfuWEBFNwnfjksegrnjkvzsgbakgfnvjnsogwiohv,mnmisehgnlsgnujweh',
+            intro:'这是一段描述',
             link:'https://a5462a0a00cdede2829495b47fe56c8d.v.smtcdns.com/om.tc.qq.com/Aa_KRUeGlCQVO8R2dxyK9y78R_5lY-wMqk0Znz4oG6bU/B_JxNyiJmktHRgresXhfyMepNBqUi0rNULHXmHzThTLbhct0S7J8ZEOLI9qBzc0jb7/svp_50001/szg_78308739_50001_3a304177e8224eecbf7f2bde40edcc26.f632.mp4?sdtfrom=v1010&guid=524690aa801f54d6&vkey=33B39EB894F0C6B147B9AFAF11939409A558F07C0E2BA57C0CAADE4E5BC0CC2D2D689A01F050EC0303CC7EC743FEC9FCBA2AF4D409BE9337BCB2DD63B9E666E0D0ACF4F04361683390532AB60FCB03782172E38A3B016CFC8C74D8F9E1C708C0657C1E0D29B94FD4D354FAE5C3DA722514BD2E5580B9D6982818701459F1EE28FE7A8D41567B807D6D8EBEA9583BAC2D9D2AB230187E98CD8D99BFC5204F98910292285CF6F5D07B'
         }
     },
@@ -31,32 +41,31 @@ export default{
 </script>
 
 <style scoped>
-.all{
+.video_container{
     background-color: white;
     border-radius: 8px;
-    width: 90%;
+    width: 1000px;
     /* margin: 24px; */
     display: flex;
-    padding: 20px;
+    /* padding: 20px; */
 }
 .title{
-    font-size: 35px;
+    font-size: 16px;
     font-weight: bold;
+    line-height:24px
 }
 .main{
-    font-size: 20px;
+    font-size: 14px;
     color: #999999;
     word-break: break-all;
     margin-top: 10px;
-    line-height: 30px;
+    line-height: 20px;
 }
 .container{
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100%;
     background-color: antiquewhite;
-    height: 100%;
     width: 80%;
 }
 video{
