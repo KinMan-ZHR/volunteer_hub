496f36308cdacd91784d6e128092899134eefe27
= <=>
2024-03-05 17:19:00 +0800

    修改userManage以适应后端提供数据

--- src/components/UserManage.vue
+++ src/components/UserManage.vue
@@ -6,7 +6,7 @@
                 <t-table
                 ref="tableRef"
                 row-key="key"
-                :data="initialData"
+                :data="data"
                 :columns="columns"
                 :editable-row-keys="editableRowKeys"
                 :pagination="pagination"
@@ -17,6 +17,7 @@
                 @row-edit="onRowEdit"
                 @row-validate="onRowValidate"
                 @validate="onValidate"
+                @page-change="onChangePage"
                 >
                 </t-table>
             </div>
@@ -64,6 +65,20 @@ export default {
                         { required: true, message: '不能为空' },
                         { max: 10, message: '字符数量不能超过 10', type: 'warning' },
                     ],
+                    showEditIcon: false,
+                },
+            },
+
+            { align: 'left', colKey: 'id', title: '用户编号', width: '120',
+                // 编辑状态相关配置，全部集中在 edit
+                edit: {
+                    // 1. 支持任意组件。需保证组件包含 `value` 和 `onChange` 两个属性，且 onChange 的第一个参数值为 new value。
+                    // 2. 如果希望支持校验，组件还需包含 `status` 和 `tips` 属性。具体 API 含义参考 Input 组件
+                    component: Input,
+                    // props, 透传全部属性到 Input 组件
+                    props: {
+                        disabled:true
+                    },
                     showEditIcon: false,
                 },
             },
@@ -255,10 +270,22 @@ export default {
                 fixed: 'right'
             },
         ])
-        const initialData= ref([
+
+        const userNum = ref(0);
+        const pageSize = ref(5);
+        const current = ref(1);
+        var initialData= [];
+
+
+        // TODO：从数据库中获取总共的用户数量
+        userNum.value = 10
+
+        // TODO：从数据库获取第一页的信息
+        initialData=[
             {
                 key: '1',
                 username: '贾明',
+                id:'1',
                 password: '624978165',
                 sex: '男',
                 maritalStatus: '宣传物料制作费用',
@@ -271,6 +298,7 @@ export default {
             {
                 key: '2',
                 username: '贾明2',
+                id:'1',
                 password: '624978165',
                 sex: '男',
                 maritalStatus: '宣传物料制作费用',
@@ -283,6 +311,7 @@ export default {
             {
                 key: '3',
                 username: '贾明3',
+                id:'1',
                 password: '624978165',
                 sex: '男',
                 maritalStatus: '宣传物料制作费用',
@@ -295,6 +324,7 @@ export default {
             {
                 key: '4',
                 username: '贾明4',
+                id:'1',
                 password: '624978165',
                 sex: '男',
                 maritalStatus: '宣传物料制作费用',
@@ -307,6 +337,7 @@ export default {
             {
                 key: '5',
                 username: '贾明5',
+                id:'1',
                 password: '624978165',
                 sex: '男',
                 maritalStatus: '宣传物料制作费用',
@@ -316,19 +347,24 @@ export default {
                 address: '扭矩输出v的女',
                 statement: '警方次哦安居房不好弄青蛙',
             },
-            {
-                key: '6',
-                username: '贾明6',
-                password: '624978165',
-                sex: '男',
-                maritalStatus: '宣传物料制作费用',
-                birthDate: '2022-01-01',
-                email: 'w.cezkdudy@lhll.au',
-                tel: '471923654912',
-                address: '扭矩输出v的女',
-                statement: '警方次哦安居房不好弄青蛙',
-            },
-        ]);
+        ];
+
+        const initData = new Array(pageSize.value).fill(null).map((_, i) => ({
+            key: String(i + 1),
+            username: initialData[i].username,
+            id:initialData[i].id,
+            password: initialData[i].password,
+            sex: initialData[i].sex,
+            maritalStatus: initialData[i].maritalStatus,
+            birthDate: initialData[i].birthDate,
+            email: initialData[i].email,
+            tel:initialData[i].tel,
+            address: initialData[i].address,
+            statement:initialData[i].statement
+
+        }));
+
+        const data = ref([...initData])
 
         const onRowEdit = (params) => {
             console.log('onrowedit',params);
@@ -347,9 +383,9 @@ export default {
         };
 
         const pagination = reactive({
-            defaultCurrent: 1,
-            defaultPageSize: 5,
-            total: initialData.value.length
+            defaultCurrent: current,
+            defaultPageSize: pageSize,
+            total: userNum
         })
 
         const onConfirmDelete = (row) =>{
@@ -360,19 +396,25 @@ export default {
             // tableRef.value.remove(row.key);
 
             // 找到要删除的元素索引
-            const indexToDelete = initialData.value.findIndex((item) => item.key === row.key);
+            const indexToDelete = data.value.findIndex((item) => item.key === row.key);
 
+            // TODO：根据indexToDelete获取用户id，根据用户id删除数据库的用户
+            console.log('要删除的id是：',data.value[indexToDelete].id);
+
+            // TODO：删除后，根据current和pageSize再次获取当前页面的数据,同时更新total的值（使用之前定义的userNum）
+            // 。。。
+
+            // ISSUE：前端效果实现逻辑在下面，后续可能要删掉
             // 删除元素
-            initialData.value.splice(indexToDelete, 1);
-           console.log("更新key前",initialData.value);
+            data.value.splice(indexToDelete, 1);
+            console.log("更新key前",data.value);
             // 更新剩余元素的 key
-            for(let i = 0; i<initialData.value.length; i++){
-                initialData.value[i].key = String(i+1)
+            for(let i = 0; i<data.value.length; i++){
+                data.value[i].key = String(i+1)
             }
 
-            pagination.total = initialData.value.length
+            pagination.total = data.value.length
 
-            console.log(initialData.value);
 
 
             MessagePlugin.success('删除成功');
@@ -419,10 +461,14 @@ export default {
                 if (params.trigger === 'parent' && !params.result.length) {
                     const current = editMap[currentSaveId.value];
                     if (current) {
-                        initialData.value.splice(current.rowIndex, 1, current.editedRow);
+                        data.value.splice(current.rowIndex, 1, current.editedRow);
                         // data[current.rowIndex]=current.editedRow
                         console.log('save:current',current);
-                        console.log('data:',initialData);
+                        console.log('data:',data);
+
+                        // TODO：将新数据data.value根据pageSize和current传输到后端
+                        // TODO（可选）：根据current和pageSize再次获取当前的页面数据
+                        // data.value,pageSize,current
 
                         MessagePlugin.success('保存成功');
                     }
@@ -441,12 +487,92 @@ export default {
             console.log('Event Table Data Validate:', params);
         }
 
+        // TODO：切换页面
+        const onChangePage = (params) => {
+            console.log('changePage',params);
+            current.value = params.current;
+            pageSize.value = params.pagesize
+
+            // TODO: 根据 current 和 pageSize 从数据库中切换页面数据...
+            var getData = [
+                {
+                    key: '1',
+                    username: '贾明5',
+                    id:'1',
+                    password: '624978165',
+                    sex: '男',
+                    maritalStatus: '宣传物料制作费用',
+                    birthDate: '2022-01-01',
+                    email: 'w.cezkdudy@lhll.au',
+                    tel: '471923654912',
+                    address: '扭矩输出v的女',
+                    statement: '警方次哦安居房不好弄青蛙',
+                },
+                {
+                    key: '2',
+                    username: '贾明6',
+                    id:'1',
+                    password: '624978165',
+                    sex: '男',
+                    maritalStatus: '宣传物料制作费用',
+                    birthDate: '2022-01-01',
+                    email: 'w.cezkdudy@lhll.au',
+                    tel: '471923654912',
+                    address: '扭矩输出v的女',
+                    statement: '警方次哦安居房不好弄青蛙',
+                },
+                {
+                    key: '3',
+                    username: '贾明7',
+                    id:'1',
+                    password: '624978165',
+                    sex: '男',
+                    maritalStatus: '宣传物料制作费用',
+                    birthDate: '2022-01-01',
+                    email: 'w.cezkdudy@lhll.au',
+                    tel: '471923654912',
+                    address: '扭矩输出v的女',
+                    statement: '警方次哦安居房不好弄青蛙',
+                },
+                {
+                    key: '4',
+                    username: '贾明8',
+                    id:'1',
+                    password: '624978165',
+                    sex: '男',
+                    maritalStatus: '宣传物料制作费用',
+                    birthDate: '2022-01-01',
+                    email: 'w.cezkdudy@lhll.au',
+                    tel: '471923654912',
+                    address: '扭矩输出v的女',
+                    statement: '警方次哦安居房不好弄青蛙',
+                },
+                {
+                    key: '5',
+                    username: '贾明9',
+                    id:'1',
+                    password: '624978165',
+                    sex: '男',
+                    maritalStatus: '宣传物料制作费用',
+                    birthDate: '2022-01-01',
+                    email: 'w.cezkdudy@lhll.au',
+                    tel: '471923654912',
+                    address: '扭矩输出v的女',
+                    statement: '警方次哦安居房不好弄青蛙',
+                },
+            ]
+            initialData = getData
+            data.value=initialData
+
+
+        }
+
 
 
         return {
             editableRowKeys,
             columns,
-            initialData,
+            data,
             tableRef,
             currentSaveId,
             editMap,
@@ -458,7 +584,8 @@ export default {
             pagination,
             onRowValidate,
             onValidate,
-            onConfirmDelete
+            onConfirmDelete,
+            onChangePage
         };
     },
 }
