3caebd794c98b2e81baf7cb9bffc460bdc7b2a2f
wubeizi <1693118245@qq.com>
2024-03-09 16:03:55 +0800

    news

--- src/components/WMHeadLine.vue
+++ src/components/WMHeadLine.vue
@@ -24,7 +24,7 @@
             <div style="width: 18px;"></div>
             <div style="flex-grow: 1;">
                 <t-space direction="vertical" :size="18" style="width: 100%;" class="headline-wo-image-container">
-                    <div class="headline-wo-image-item" v-for="(item,index) in headline.list2" :key="index" >
+                    <div class="headline-wo-image-item" v-for="(item,index) in headline.list2" :key="index" @click="onClickToReadNews(index)">
                         <!-- :style="{ background: 'url(' + item.image + ') no-repeat center' }" style="background-size: cover;" -->
                         {{ item.title }}
                     </div>
@@ -32,15 +32,34 @@
             </div>
         </div>
 
+        <t-dialog
+            v-model:visible="news_visible"
+            header="文章详情"
+            body="对话框内容"
+            :cancel-btn="null"
+            width="90%"
+            top="50px"
+            :confirm-btn="null"
+        >
+        <template #header>
+            <t-icon name="component-space"></t-icon>
+            <p>文章详情</p>
+        </template>
+            <WenZhang :current_article="current_news"></WenZhang>
+        </t-dialog>
     </div>
 </template>
 
 <script>
 import { ref, reactive, onMounted } from 'vue';
 import {getHeadlineListAPI} from '@/apis/headlinesHandler.js';
+import WenZhang from './WenZhang.vue';
+
 export default {
   name: 'WMHeadLine',
-  components: {},
+  components: {
+    WenZhang
+  },
   setup() {
     const headline = reactive({
       list: [],
@@ -88,6 +107,24 @@ export default {
       hover,
       unhover
     };
+  },
+  data(){
+    return{
+      news_visible:false,
+      current_news:{
+          title:'文章标题2',
+          time:'2024-03-01',
+          source:'新华社',
+          text:'必须把坚持高质量发展作为新时代的硬道理”——习近平总书记在去年中央经济工作会议上深刻总结新时代做好经济工作的“五个必须”规律性认识，其中这个“硬道理”居于首位。\n今年是新中国成立75周年，是实现“十四五”规划目标任务的关键一年。在这个重要年份召开的全国两会，如何聚焦高质量发展这个硬道理，进一步擘画出推进高质量发展的路线图、施工图，备受各方关注。',
+          image:'https://ts1.cn.mm.bing.net/th/id/R-C.171e8fe1aa1544a1868ab710eed82d82?rik=FLPxvVVL9C9bnQ&riu=http%3a%2f%2fwww.pp3.cn%2fuploads%2fallimg%2f200710%2f14-200G00Z321.jpg&ehk=Lb0IHCCZIdqYQOi28m%2borU8c1ARGbTEC%2f8WYzfwRuHo%3d&risl=&pid=ImgRaw&r=0'
+      }
+    }
+  },
+  methods:{
+    onClickToReadNews(index){
+      this.current_news = this.headline.list2[index];
+      this.news_visible = true;
+    }
   }
 };
 </script>
